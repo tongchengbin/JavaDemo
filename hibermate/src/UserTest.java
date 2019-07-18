efe4fd5f63f63ae9bd60aba07db874cbb3a7305c
tongchengbin <58296672@qq.com>
2019-07-18 11:01:22 +0800

    一级缓存

--- hibermate/src/UserTest.java
+++ hibermate/src/UserTest.java
@@ -5,22 +5,31 @@ import org.hibernate.cfg.Configuration;
 public class UserTest {
 
     public static void main(String[] args) {
+        test2();
+    }
+
+    static void test2(){
+
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
 
         Session s = sf.openSession();
         s.beginTransaction();
-
-        User p = (User)s.load(User.class, 1);
+        User p1 = (User) s.get(User.class, 1);
         System.out.println("log1");
-        System.out.println(p.getName());
-
-
-        User p2 = (User)s.load(User.class, 1);
+        User p2 = (User) s.get(User.class, 1);
         System.out.println("log2");
-        System.out.println(p2.getId());
-
         s.getTransaction().commit();
         s.close();
+        Session s2 = sf.openSession();
+        s2.beginTransaction();
+        User p3 = (User) s2.get(User.class, 1);
+        System.out.println("log3");
+        s2.getTransaction().commit();
+        s2.close();
         sf.close();
+
     }
+
+
+
 }
